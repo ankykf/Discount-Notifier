31a61237e0448160d280fae1fe176c9503375329
Kevin Fang <58371107+k-fang@users.noreply.github.com>
2020-05-08 17:47:48 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,15 +7,10 @@ import ItemsList
 from urllib.error import HTTPError
 
 def getPrice(url):
-
-
     res = requests.get(url, headers={ "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
     res.raise_for_status()
-
     soup = bs4.BeautifulSoup(res.content, "lxml")
-
     elem = soup.select('#prcIsum')
-
     return elem[0].text.strip()
 
 itemsList = ItemsList.ItemsList()
@@ -29,7 +24,6 @@ while (enterLoop):
     newOrExisting = input("Type the item name that you would like to search: ")
     while (urlCheck):
         userInput = input("What is the link to your item? (enter everything that appears after the .ca/) ")
-      
         baseUrl = "https://www.ebay.ca/"
         completeUrl = baseUrl + userInput
         try:
@@ -39,7 +33,6 @@ while (enterLoop):
             print ("That's not a valid URL, try again. ")
               
     newprice= price.lstrip("CND$ ")
-
     itemsList.addItem(newOrExisting, newprice)
     while (check):
         runAgain = input("Would you like to look up another item (Type Yes or No): ")
